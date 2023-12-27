4fe7d535eb8b563ad5b80b7afad9356da03d433c
shmzzzz <slt0623@gmail.com>
2023-12-27 21:13:38 +0900

    リストアイテムのUIの実装

--- app/src/main/java/com/example/imagesearchapp/presentation/search_images/components/ImageThumbnail.kt
+++ app/src/main/java/com/example/imagesearchapp/presentation/search_images/components/ImageThumbnail.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -18,8 +19,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.imagesearchapp.domain.model.Image
+import com.example.imagesearchapp.presentation.ui.theme.ImageSearchAppTheme
 
 @Composable
 fun ImageThumbnail(
@@ -32,6 +36,14 @@ fun ImageThumbnail(
             .clickable { onClick(image) },
         contentAlignment = Alignment.BottomCenter,
     ) {
+        // 先にローディングが表示される
+        CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        // AsyncImageが実行されるときはローディング画面が非表示になる
+        AsyncImage(
+            model = image.imageUrl,
+            contentDescription = image.description,
+            modifier = Modifier.fillMaxWidth()
+        )
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -65,5 +77,20 @@ fun ImageThumbnail(
                 style = MaterialTheme.typography.bodyMedium,
             )
         }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewImageThumbnail() {
+    val image = Image(
+        imageId = "",
+        description = "Image description",
+        likes = 100,
+        imageUrl = "https://images.unsplash.com/photo-1587620962725-abab7fe55159?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8fDA%3D",
+        photographer = "Surface",
+    )
+    ImageSearchAppTheme {
+        ImageThumbnail(image = image, onClick = {})
     }
 }
