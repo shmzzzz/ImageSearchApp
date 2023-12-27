063b93d221635173e089dc05a380ed81be6d5ff7
shmzzzz <slt0623@gmail.com>
2023-12-27 12:02:49 +0900

    画像詳細情報取得用の関数を定義する

--- app/src/main/java/com/example/imagesearchapp/data/remote/UnsplashApi.kt
+++ app/src/main/java/com/example/imagesearchapp/data/remote/UnsplashApi.kt
@@ -3,6 +3,7 @@ package com.example.imagesearchapp.data.remote
 import com.example.imagesearchapp.common.Constants
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface UnsplashApi {
@@ -10,4 +11,8 @@ interface UnsplashApi {
     @Headers("Authorization: Client-ID ${Constants.API_KEY}")
     @GET("/search/photos")
     suspend fun searchImages(@Query("query") query: String): SearchImagesResultDto
+
+    @Headers("Authorization: Client-ID ${Constants.API_KEY}")
+    @GET("/photos/{id}")
+    suspend fun getImageById(@Path("id") imageId: String): ImageDetailDto
 }
