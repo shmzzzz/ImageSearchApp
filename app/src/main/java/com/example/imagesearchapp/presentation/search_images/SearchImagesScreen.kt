a21ba71dd63fbb5b283820a15371d8a902a44f67
shmzzzz <slt0623@gmail.com>
2023-12-27 17:37:46 +0900

    画像検索画面の簡易的なUIの作成

---
+++ app/src/main/java/com/example/imagesearchapp/presentation/search_images/SearchImagesScreen.kt
@@ -0,0 +1,21 @@
+package com.example.imagesearchapp.presentation.search_images
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@Composable
+fun searchImagesScreen(
+    viewModel: SearchImagesViewModel = hiltViewModel(),
+) {
+    // ViewModelが管理しているStateを参照する
+    val state = viewModel.state.value
+    // リスト表示を作成する
+    LazyColumn {
+        items(state.images) { image ->
+            Text(text = image.imageUrl)
+        }
+    }
+}
