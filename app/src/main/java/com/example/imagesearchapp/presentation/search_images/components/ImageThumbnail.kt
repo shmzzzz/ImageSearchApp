4e1cada216569cb3e2e1ad5ca0355283311781fe
shmzzzz <slt0623@gmail.com>
2023-12-27 19:30:50 +0900

    ImageThumbnailコンポーネント作成(途中)

---
+++ app/src/main/java/com/example/imagesearchapp/presentation/search_images/components/ImageThumbnail.kt
@@ -0,0 +1,52 @@
+package com.example.imagesearchapp.presentation.search_images.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.imagesearchapp.domain.model.Image
+
+@Composable
+fun ImageThumbnail(
+    image: Image,
+    onClick: (Image) -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .background(Color.Black)
+            .clickable { onClick(image) },
+        contentAlignment = Alignment.BottomCenter,
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.Black.copy(alpha = 0.5f))
+                .padding(10.dp),
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = image.description ?: "No description",
+                    color = Color.White,
+                    style = MaterialTheme.typography.labelSmall,
+                )
+                Text(
+                    text = image.photographer ?: "Unknown photographer",
+                    color = Color.White,
+                    style = MaterialTheme.typography.bodySmall,
+                )
+            }
+        }
+    }
+}
