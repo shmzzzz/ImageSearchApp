e0dc9c86093020f29e241930df99a7b0958da62b
shmzzzz <slt0623@gmail.com>
2023-12-27 16:21:36 +0900

    画面遷移ができるように設定する

--- app/src/main/java/com/example/imagesearchapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/imagesearchapp/presentation/MainActivity.kt
@@ -6,7 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.imagesearchapp.presentation.ui.theme.ImageSearchAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +23,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        // 画像検索画面を最初に表示する
+                        startDestination = ScreenRoute.SearchImageScreen.route,
+                    ) {
+                        // NavHostBuilderは最後の引数かつ関数なのでNavHostBuilderは外に出せる
+                        // 画像検索画面
+                        composable(route = ScreenRoute.SearchImageScreen.route) {
+                            // TODO
+                        }
+                        // 画像詳細画面
+                        composable(route = ScreenRoute.ImageDetailScreen.route) {
+                            // TODO
+                        }
+                    }
                 }
             }
         }
