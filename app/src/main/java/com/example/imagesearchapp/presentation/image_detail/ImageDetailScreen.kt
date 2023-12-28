8b5064fc09d02184826b63156d3d943517c61876
shmzzzz <slt0623@gmail.com>
2023-12-28 12:09:39 +0900

    他の表示要素の追加

--- app/src/main/java/com/example/imagesearchapp/presentation/image_detail/ImageDetailScreen.kt
+++ app/src/main/java/com/example/imagesearchapp/presentation/image_detail/ImageDetailScreen.kt
@@ -2,12 +2,18 @@ package com.example.imagesearchapp.presentation.image_detail
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -19,10 +25,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.example.imagesearchapp.domain.model.ImageDetail
+import com.example.imagesearchapp.presentation.components.CountLabel
 
 @Composable
 fun ImageDetailScreen(
@@ -88,6 +96,31 @@ fun ImageDetailContent(
                 // 通信成功時はローディングを完了させる
                 onSuccess = { isLoadingImage = false }
             )
+        }
+        Column(modifier = Modifier.padding(10.dp)) {
+            Text(
+                text = imageDetail.description ?: "No description",
+                style = MaterialTheme.typography.labelMedium,
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            Text(
+                text = imageDetail.photographer ?: "Unknown photographer",
+                style = MaterialTheme.typography.bodySmall,
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            CountLabel(
+                imageVector = Icons.Default.Favorite,
+                count = imageDetail.likes ?: 0,
+                iconTint = Color.Magenta,
+            )
+            CountLabel(
+                imageVector = Icons.Default.Share,
+                count = imageDetail.downloads ?: 0,
+                iconTint = Color.Green
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            Text(text = "Camera: ${imageDetail.camera}")
+            Text(text = "Location: ${imageDetail.location}")
         }
     }
 }
