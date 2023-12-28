cb20265ea70b01ee151ae4359e6b7441e4750d29
shmzzzz <slt0623@gmail.com>
2023-12-28 11:59:41 +0900

    通信成功時の表示

--- app/src/main/java/com/example/imagesearchapp/presentation/image_detail/ImageDetailScreen.kt
+++ app/src/main/java/com/example/imagesearchapp/presentation/image_detail/ImageDetailScreen.kt
@@ -1,16 +1,24 @@
 package com.example.imagesearchapp.presentation.image_detail
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
+import com.example.imagesearchapp.domain.model.ImageDetail
 
 @Composable
 fun ImageDetailScreen(
@@ -41,6 +49,32 @@ fun ImageDetailScreen(
                     )
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun ImageDetailContent(
+    imageDetail: ImageDetail
+) {
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState())
+    ) {
+        Box(modifier = Modifier.heightIn(min = 200.dp)) {
+            AsyncImage(
+                model = imageDetail.imageUrl,
+                contentDescription = imageDetail.description,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(
+                        RoundedCornerShape(
+                            topStartPercent = 0,
+                            topEndPercent = 0,
+                            bottomStartPercent = 5,
+                            bottomEndPercent = 5,
+                        )
+                    )
+            )
         }
     }
 }
