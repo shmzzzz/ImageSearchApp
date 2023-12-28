0926fca4e4ddfc59b8df3c4bae1c86f86f110460
shmzzzz <slt0623@gmail.com>
2023-12-28 10:58:12 +0900

    画像検索画面の設定

--- app/src/main/java/com/example/imagesearchapp/presentation/search_images/SearchImagesScreen.kt
+++ app/src/main/java/com/example/imagesearchapp/presentation/search_images/SearchImagesScreen.kt
@@ -13,11 +13,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
+import com.example.imagesearchapp.presentation.ScreenRoute
 import com.example.imagesearchapp.presentation.search_images.components.ImageThumbnail
 import com.example.imagesearchapp.presentation.search_images.components.SearchBar
 
 @Composable
 fun SearchImagesScreen(
+    navController: NavController,
     viewModel: SearchImagesViewModel = hiltViewModel(),
 ) {
     // ViewModelが管理しているStateを参照する
@@ -54,7 +57,13 @@ fun SearchImagesScreen(
                     // 通信成功しているのでリストを表示する
                     LazyColumn(modifier = Modifier.padding(paddingValue)) {
                         items(state.images) { image ->
-                            ImageThumbnail(image = image, onClick = {})
+                            ImageThumbnail(
+                                image = image,
+                                onClick = {
+                                    // 指定したリンクへ遷移する
+                                    navController.navigate(ScreenRoute.ImageDetailScreen.route + "/${image.imageId}")
+                                },
+                            )
                         }
                     }
                 }
