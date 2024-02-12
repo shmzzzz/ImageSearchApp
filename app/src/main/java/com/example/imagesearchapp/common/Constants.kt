0414d831119cca62e7c4e3b78190f44997766b07
shmzzzz <slt0623@gmail.com>
2024-02-13 05:31:51 +0900

    APIキーのコメントを追加

--- app/src/main/java/com/example/imagesearchapp/common/Constants.kt
+++ app/src/main/java/com/example/imagesearchapp/common/Constants.kt
@@ -1,6 +1,7 @@
 package com.example.imagesearchapp.common
 
 object Constants {
+    // このAPIキーはdisabledにしているので、再度確認したい場合はAPIを再発行する必要がある
     const val API_KEY = "nY3wgIsqt4pHCbTOxzOb6NsRmA09UPSDoeSqfYlMKxk"
     const val BASE_URL = "https://api.unsplash.com"
 }
