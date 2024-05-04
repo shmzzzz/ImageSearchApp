206a503249c1e757a8b3098e154c84e7e666c9fe
shmzzzz <slt0623@gmail.com>
2024-05-04 16:16:37 +0900

    build: hiltのversionを揃える

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,5 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
-    val hiltVersion = "2.44"
-    id("com.google.dagger.hilt.android") version "$hiltVersion" apply false
+    id("com.google.dagger.hilt.android") version "2.49" apply false
 }
